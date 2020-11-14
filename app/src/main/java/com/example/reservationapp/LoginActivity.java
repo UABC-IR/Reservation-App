d8e755a143cac0266368473199d643324761cee1
Diego <diego.caudillo@uabc.edu.mx>
2020-11-14 12:56:20 -0800

    Login and Register UI

---
+++ app/src/main/java/com/example/reservationapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.reservationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btn_register;
+    Button btn_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.button_login_signIn);
+        btn_register = findViewById(R.id.button_login_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+}
