1612df77a745814d3060d53408d42aaa034c662b
Diego <diego.caudillo@uabc.edu.mx>
2020-12-05 13:35:37 -0800

    login validation

--- app/src/main/java/com/example/reservationapp/LoginActivity.java
+++ app/src/main/java/com/example/reservationapp/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.example.reservationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth fAuth;
     Button btn_register;
     Button btn_login;
+    EditText etEmail, etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +30,9 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_login = findViewById(R.id.button_login_signIn);
         btn_register = findViewById(R.id.button_login_register);
+        etEmail = findViewById(R.id.editText_login_user);
+        etPassword = findViewById(R.id.editText_login_password);
+        fAuth = FirebaseAuth.getInstance();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +44,36 @@ public class LoginActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                if(TextUtils.isEmpty( email )){
+                    etEmail.setError("Invalid Email");
+                    return;
+                }
+
+                if(TextUtils.isEmpty( password )){
+                    etPassword.setError("Invalid Password");
+                    return;
+                }
+                if(password.length() < 6) {
+                    etPassword.setError("Password must be at least 6 chars long");
+                    return;
+                }
+
+                // Auth user info
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in succesfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Error: Invalid Credentials" , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
+
 }
