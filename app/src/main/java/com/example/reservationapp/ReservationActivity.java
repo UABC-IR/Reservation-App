8a619d9b4acd4ab85c50560a306adeace5b2fb13
Diego <diego.caudillo@uabc.edu.mx>
2020-12-05 12:51:17 -0800

    Firebase Sync (Gradle)

--- app/src/main/java/com/example/reservationapp/ReservationActivity.java
+++ app/src/main/java/com/example/reservationapp/ReservationActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReservationActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class ReservationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
